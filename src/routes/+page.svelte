14274f114b26cdc73533bec2db22a4bae4de661d
Elijah <eliyang256@outlook.com>
2024-01-09 19:07:05 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="text-3xl font-bold underline">Hello world! <span>Eli</span></h1>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+    h1{
+        @apply text-purple-600;
+    }
+    span{
+        @apply text-amber-600;
+    }
+</style>
